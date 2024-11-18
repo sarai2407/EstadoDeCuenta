3e12d96c64a39e379bcaa82eacd8438929eccefa
sarai2407 <123722223+sarai2407@users.noreply.github.com>
2024-11-18 07:14:40 -0600

    Update SQLQuery_insert_PrimerosDatos.sql

--- EstadoCuenta.Data/Scripts/SQLQuery_insert_PrimerosDatos.sql
+++ EstadoCuenta.Data/Scripts/SQLQuery_insert_PrimerosDatos.sql
@@ -2,13 +2,14 @@ use EstadoCuentaDB
 
 -- Insertar un usuario
 INSERT INTO Usuario (Nombre, Apellidos, Telefono, Email, Dui)
-VALUES ('Juan', 'Pérez', '12345678', 'jperez@gmail.com', '12345678-9');
+VALUES ('Juan', 'PÃ©rez', '12345678', 'jperez@gmail.com', '12345678-9');
 
 -- Insertar una tarjeta
 INSERT INTO Tarjeta (NumTarjeta, LimiteCredito, Saldo, SaldoDisponible, IdUsuario)
 VALUES ('1234567812345678', 1500.00, 100.00, 1400.00, 1);
 
--- Insertar los tipos de transacción (Compra y Deposito)
+-- Insertar los tipos de transacciÃ³n (Compra y Deposito)
+-- compra tiene que ser el IdTipoTransaccion = 1  y Deposito el IdTipoTransaccion = 2
 INSERT INTO TipoTransaccion (TipoDeTransaccion) VALUES
 ('Compra'),
 ('Deposito');
@@ -17,6 +18,6 @@ INSERT INTO TipoTransaccion (TipoDeTransaccion) VALUES
 INSERT INTO Transaccion (Fecha, Monto, Descripcion, SaldoDisponible, IdTipoTransaccion, NumTarjeta)
 VALUES (2024-11-12 15:43:39.6770000 , 200.00, 'Compra en tienda A', 1300.00, 1, '1234567812345678');
 
--- Insertar un depósito
+-- Insertar un depÃ³sito
 INSERT INTO Transaccion (Fecha, Monto, Descripcion, SaldoDisponible, IdTipoTransaccion, NumTarjeta)
 VALUES (2024-11-15 18:43:39.6770000 , 100.00, 'Pago de tarjeta', 1400.00, 2, '1234567812345678');
