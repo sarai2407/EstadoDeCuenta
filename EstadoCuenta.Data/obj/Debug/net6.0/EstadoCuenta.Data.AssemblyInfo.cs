efba09621e96c2164ab52668ccba83953ed5c0e0
sarai2407 <123722223+sarai2407@users.noreply.github.com>
2024-11-18 02:36:19 -0600

    agregando script para insertar los primeros datos

--- EstadoCuenta.Data/obj/Debug/net6.0/EstadoCuenta.Data.AssemblyInfo.cs
+++ EstadoCuenta.Data/obj/Debug/net6.0/EstadoCuenta.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EstadoCuenta.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+030bd87bcf4485137a78af1494d400eb248062f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b77304577bcdccf5c2841dd0a7efed401ea6278c")]
 [assembly: System.Reflection.AssemblyProductAttribute("EstadoCuenta.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EstadoCuenta.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
